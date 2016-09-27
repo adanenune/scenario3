0fbcda9aa2d0959617c43a99183c6d1cbd4788dc
carlomarcoli <carlomarcoli@yahoo.it>
2016-09-27 12:00:10 +0100

    added loopback code

---
+++ order-tracker microservice/order-tracker/common/models/message-consumer.js
@@ -0,0 +1,48 @@
+module.exports = function(Messageconsumer) {
+
+  Messageconsumer.remoteMethod('startPolling',
+        {
+            http: {path:'/start', verb:'post'},
+            returns: {arg:'state',vtype:'object'}
+        }
+  );
+
+  Messageconsumer.startPolling = function(callback) {
+    Messageconsumer.app.models.MessageHubProxy.startComsumer("order_events",function(err, emitter){
+      callback(err, "polling about to start...");
+      emitter.on('order-event',function(data){
+          console.log("data received by the emitter: " + data);
+          for(var i=0; i<data.length; i++){
+            myevent = JSON.parse(data[i]);
+            newOder = {
+              customerId : myevent.customerId,
+              description : myevent.description,
+              orderId : myevent.orderId,
+            };
+            newEvent = {
+              orderId : myevent.orderId,
+              eventId : myevent.eventId,
+              time : myevent.time,
+              type : myevent.type,
+            };
+            Messageconsumer.app.models.Order.findById(myevent.orderId,function(err,instance){
+              if (err){
+                throw err;
+              } else {
+                if (instance == null){
+                  Messageconsumer.app.models.Order.create(newOder, function(err, object){
+                    console.log("new order created: " + JSON.stringify(object));
+                  });
+                }
+                Messageconsumer.app.models.OrderEvent.create(newEvent, function(err, object){
+                    console.log("new order-event created: " + JSON.stringify(object));
+                });
+              }
+            });
+          }
+      });
+    });
+  };
+
+
+};
---
+++ order-tracker microservice/order-tracker/common/models/message-hub-proxy.js
@@ -0,0 +1,82 @@
+
+var MessageHub = require('message-hub-rest');
+var events = require('events');
+
+module.exports = function(Messagehubproxy) {s
+
+  var defaultProperties =
+  {
+    "messagehub": [
+    {
+      "name": "Message Hub-hk",
+      "label": "messagehub",
+      "plan": "standard",
+      "credentials":   {
+          "mqlight_lookup_url": "https://mqlight-lookup-prod02.messagehub.services.eu-gb.bluemix.net/Lookup?serviceId=efbc8efb-920e-4ce0-adcb-77a10c22002f",
+          "api_key": "i76hACf78WCmpWlp3owfezyyiYmAnF9l6xWpYTzHVVcxy1FQ",
+          "kafka_admin_url": "https://kafka-admin-prod02.messagehub.services.eu-gb.bluemix.net:443",
+          "kafka_rest_url": "https://kafka-rest-prod02.messagehub.services.eu-gb.bluemix.net:443",
+          "kafka_brokers_sasl": [
+            "kafka01-prod02.messagehub.services.eu-gb.bluemix.net:9093",
+            "kafka02-prod02.messagehub.services.eu-gb.bluemix.net:9093",
+            "kafka03-prod02.messagehub.services.eu-gb.bluemix.net:9093",
+            "kafka04-prod02.messagehub.services.eu-gb.bluemix.net:9093",
+            "kafka05-prod02.messagehub.services.eu-gb.bluemix.net:9093"
+          ],
+          "user": "xxxx",
+          "password": "xxxx"
+      }
+   }]
+  }
+
+  var services = process.env.VCAP_SERVICES || defaultProperties;
+
+  this.hubInstance = new MessageHub(services);
+
+  Messagehubproxy.topics = function(cb){
+    console.log(JSON.stringify(services));
+    hubInstance.topics.get()
+      .then(function(result) {
+        cb (null, result);
+      })
+      .fail(function(error) {
+        console.error(error);
+        cb (err, null);
+      });
+  };
+
+  Messagehubproxy.produce = function (message, topic, cb){
+    var list = new MessageHub.MessageList([message]);
+    hubInstance.produce(topic, list.messages)
+      .then(function(response){
+          console.log(response);
+          cb(null,response);
+      })
+      .fail(function(error){
+          console.log(error.stack);
+          cb(error,null);
+      });
+  };
+
+  Messagehubproxy.startComsumer = function (topic, cb){
+    var consumer;
+    var receivedMessage = 0;
+    var mhEventEmitter = new events.EventEmitter();
+    cb(null,mhEventEmitter);
+    hubInstance.consume('order-tracker-group', 'order-tracker-c1', {'auto.offset.reset':'largest'})
+      .then(function(response) {
+        consumer = response[0];
+        var consumeInterval = setInterval(function () {
+          consumer.get(topic)
+            .then(function(data){
+              receivedMessage++;
+              mhEventEmitter.emit('order-event',data);
+            })
+            .fail(function(error){
+              throw new Error(error);
+            })
+        }, 10000);
+      })
+  };
+
+};
