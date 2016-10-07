6b910805cf392cda9b728b8ac796a8f6ca098402
Carsten Börnert <carsten.bornert@uk.ibm.com>
2016-10-07 08:27:47 -0700

    added network alias and improved test script

--- 99_docker/iib/testme.sh
+++ 99_docker/iib/testme.sh
@@ -1,4 +1,7 @@
 echo
+echo "Test command used:"
+echo "curl -X POST -d @test_event1.json -H "Content-Type: application/json" http://localhost:7800/orderupdates"
+echo
 curl -X POST -d @test_event1.json -H "Content-Type: application/json" http://localhost:7800/orderupdates
 echo
 echo
